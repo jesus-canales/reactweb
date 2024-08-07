21530d2257f3ea7abb40fed1ebb8cffb22d288e6
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-07 19:29:12 +0000

    Avanzamos creando un primer componente con bootstrap

---
+++ react-app-vite/src/components/ListGroup.jsx
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+
+function ListGroup() {
+
+    return (
+        
+        <>
+            <h1>Esta es una lista de elementos con bootstrap</h1>
+            <ul class="list-group">
+                <li class="list-group-item">Soy el primer elemento</li>
+                <li class="list-group-item">A second item</li>
+                <li class="list-group-item">A third item</li>
+                <li class="list-group-item">A fourth item</li>
+                <li class="list-group-item">And a fifth one</li>
+            </ul>
+        </>
+    );
+}
+
+export default ListGroup;
